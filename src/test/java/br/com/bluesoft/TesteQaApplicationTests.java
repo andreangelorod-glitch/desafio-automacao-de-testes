e9b876f8dab0ee1f79d41921f784f558c836d25b
Andre <andre.angelo.rod@gmail.com>
2025-08-28 16:11:21 -0300

    init

---
+++ src/test/java/br/com/bluesoft/TesteQaApplicationTests.java
@@ -0,0 +1,20 @@
+package br.com.bluesoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+
+}
+
+
+
+	}
+
+
